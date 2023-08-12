68fa0b8317361ef2769c42cd4af8ab78eb4b4d5e
cbasoa <141192086+cbasoa@users.noreply.github.com>
2023-08-11 21:04:27 -0300

    solucion-click
    Esta sí es la solución

--- eventos.js
+++ eventos.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById("boton").addEventListener("click", function(){
+   
     alert("Hola! Soy el div");
+    
 });
 
 
